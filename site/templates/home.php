1bc6d88e46c0dd4f45c709fe9bbe1a0dd2d729aa
Isha van Baar <isha.vanbaar@gmail.com>
2016-05-16 20:27:51 +0200

    Full screen map

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,11 +1,26 @@
 <?php snippet('header') ?>
-	
-	<!-- <div class="container" role="main"> -->
-	<!-- Search bar -->
-	<?php snippet('search') ?>
-	
-	<!-- RECENTLY ADDED -->
-    <?php snippet('recently-added') ?>
-</div>
+    <body>
+        <div class="container" role="main">
+            <div class="row">
+                <div id="map"</div>
+            </div>
+        </div>
+        
+        <!-- MAPS scripts -->
+        <script src="https://maps.googleapis.com/maps/api/js?callback=initMap&key=AIzaSyBSRXd-KxP4MMM04uSk3kpO-72q380UBAc" 
+                async defer>
+        </script>
 
-<?php snippet('footer') ?>
+        <script>
+            var map;
+            
+            function initMap(zoomLevel) {
+                var mapDiv = document.getElementById('map');
+                map = new google.maps.Map(mapDiv, {
+                  center: {lat: 44.540, lng: -78.546},
+                  zoom: 8
+                });
+            }
+        </script>   
+    </body>
+</html>
