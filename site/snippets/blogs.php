5058bff0e75bd77e596dd0c6d361fa7cf2aa051a
Isha van Baar <isha.vanbaar@gmail.com>
2016-05-18 00:38:03 +0200

    Added blog parts to the info pointers

--- site/snippets/blogs.php
+++ site/snippets/blogs.php
@@ -1,20 +1,29 @@
-<!-- Puts entries in thumbnails-->
 <?php 
 
-$blogs = $page('blogs')->children()->visible()->limit(200);
+$blogs = page('blogs')->children()->visible()->limit(200);
 
 foreach($blogs as $blog): 
     $blog_title = $blog->title()->html();
-    $blog_text = $blog->blog()->kirbytext();
-    $blog_main_image = $blog->image((string)$blog->main_image());
+    $blog_text = $blog->blog()->kirbytext()->excerpt(300); //now takes excerpt of first 300 chars.
+    $blog_main_image = $blog->image((string)$blog->main_image())->url();
+    $blog_url = $blog->url();
 ?>
-
-    <div id="content">
+    
+    <!-- INFO WINDOW -->
+    <div class="infowindow" style="display: none;">
         <div id="siteNotice"></div>
-        <h1 id="firstHeading" class="firstHeading"><?php echo $blog_title ?></h1>
-        <div id="bodyContent">
-            <p><?php echo $blog_text; ?></p>
-        </div>
+        <a href="<?php echo $blog_url ?>">
+            <!-- MAIN IMAGE AND TITLE -->
+            <img src="<?php echo $blog_main_image ?>" alt="">
+            <h2 class="caption"><?php echo $blog_title ?></h2>
+            
+            <!-- MAIN CONTENT -->
+            <div id="bodyContent">
+                <p><?php echo $blog_text; ?></p>
+            </div>
+        </a>
+        
+
     </div>
 
 <?php endforeach ?>
