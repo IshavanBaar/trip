0af71c14f8d5d2d40c6a319929e2e55374965070
Isha van Baar <isha.vanbaar@gmail.com>
2016-06-18 23:04:26 +0200

    Slight change

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,21 +7,21 @@
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
         <!-- CSS -->
-        <?php echo css('assets/css/bootstrap.css') ?>
-        <?php echo css('assets/css/main.css') ?>
-
-        <!-- MODERNIZER FOR OLD BROWSERS -->	
-        <?php echo js('assets/js/lib/modernizr-2.8.3-respond-1.4.2.min.js') ?>
+        <?php echo css(array(
+            'assets/css/bootstrap.css',
+            'assets/css/main.css'
+        ))?>
         
-        <!-- Jquery, bootstrap, and main js -->
+        <!-- Modernizer, Jquery, bootstrap, maps style, and main js -->
         <?php echo js(array(
+            'assets/js/lib/modernizr-2.8.3-respond-1.4.2.min.js',
             'assets/js/lib/jquery-1.12.3.js',
             'assets/js/lib/bootstrap.min.js',
             'assets/js/mapstyles/gray-white-style.js',
             'assets/js/maps.js'
         ))?>
         
-        <!-- FONTS -->
+        <!-- FONT -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:700" rel="stylesheet"> 
     </head>
 
